50811b8c09b2c3b1cbb7cbe6f220c2473bfc32ca
aaron <aschuettpelz@ethz.ch>
2026-04-05 19:11:53 +0200

    chore(storage): Added logging

--- src/storage/disk_manager.cpp
+++ src/storage/disk_manager.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <cassert>
 #include <span>
+#include <spdlog/spdlog.h>
 
 const int PAGE_SIZE = 4096; //ToDo: Store in seperate constants file
 
@@ -13,23 +14,30 @@ class DiskManager{
 
 public:
     explicit DiskManager(const std::string& db_path){
+        spdlog::info("Opening database file: {}", db_path);
 
         //Try open the file with read,write permissions and no interpretation of the data
         db_io_.open(db_path, std::ios::in | std::ios::out | std::ios::binary);
 
         //if open failed create a new file and open
         if(!db_io_.is_open()){
+            spdlog::warn("File not found, creating: {}", db_path);
             db_io_.open(db_path, std::ios::trunc | std::ios::out | std::ios::binary);
             db_io_.close();
             
             db_io_.open(db_path, std::ios::in | std::ios::out | std::ios::binary);
+            
+            if(!db_io_.is_open()){
+                spdlog::error("Critical: Could not create file {}", db_path);
+            }
         }
-        
     }
 
     // Write a page to the database
     void write_page(const u_int32_t page_id, const char* payload){ //ToDo: maybe define Datatype for page indices
         size_t page_offset = page_id * PAGE_SIZE;
+        spdlog::debug("Writing page {} at offset {}", page_id, page_offset);
+        
         db_io_.seekp(page_offset);
         db_io_.write(payload, PAGE_SIZE);
 
@@ -38,11 +46,12 @@ public:
 
     // read a page from the database. We use span to make sure we have the right size
     void read_page(size_t page_id, std::span<char, PAGE_SIZE> buffer) {
-        db_io_.seekg(page_id * PAGE_SIZE);
+        size_t page_offset = page_id * PAGE_SIZE;
+        spdlog::debug("Reading page {} from offset {}", page_id, page_offset);
+
+        db_io_.seekg(page_offset);
         db_io_.read(buffer.data(), PAGE_SIZE);
     }   
-
-
 
 private:
     std::fstream db_io_;
